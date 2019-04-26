d353ee3e8e1ff1d0baea7247460815f26f3df19b
Jurshsmith <jurshsmith@gmail.com>
2019-04-26 08:25:51 +0100

    handled page 404

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ server.use(express.static(__dirname + '/client'));
 server.use('/api', require('./app/routes/search.route'));
 
 server.get('/:page', function(req,res) {
-    res.sendFile(path.join(__dirname+`/client/${req.params.page}.html`));
+    const config = require('./common/route.config');
+    const file = config[req.params.page];
+
+    if (file){
+        return res.sendFile(path.join(__dirname+`/client/${file}.html`));
+    }
+    return res.redirect('/NotFound');
 });
 
 server.listen(PORT, () => {
